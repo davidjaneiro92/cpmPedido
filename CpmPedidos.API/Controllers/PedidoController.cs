1fbf3f3d40aeb2571c741526ae5acd7306c35965
David <davidjaneiro92@gmail.com>
2022-04-30 21:21:41 -0300

    test

---
+++ CpmPedidos.API/Controllers/PedidoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CpmPedidos.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PedidoForecastController : ControllerBase
+    {
+        
+        public PedidoForecastController()
+        {
+            
+        }
+
+       
+    }
+}
